e43b2f39b069e6c06087c088663b0b1e1dd108f8
Antonio Lazaro Borges <antonio.lazaro@gmail.com>
2020-05-03 09:42:54 -0300

    novos testes e experimentos

---
+++ demo-plural-sight-gradle/src/main/kotlin/demo/objects/Coroutine.kt
@@ -0,0 +1,16 @@
+package demo.objects
+
+
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    for (i in 0..1000 step 200) {
+        launch {
+            delay(1000L - i)
+            print(i)
+        }
+    }
+}
